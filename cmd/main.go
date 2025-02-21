f3bb87f8ca5035c28b8b43e426e7e2c469fc6426
Andrey Kudishkin <kudyshkin.a.k@mail.ru>
2025-02-21 19:50:36 +0300

    Add config parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"todo/internal/config"
 	"todo/internal/handlers"
 	inmemory "todo/internal/storage/in_memory"
 )
 
 func main() {
+	cfg := config.New()
+
+	fmt.Println(cfg)
+
+	_ = cfg
+
 	storage := inmemory.NewStorage()
 
 	http.HandleFunc("/", handlers.CreateTaskHandler(storage))
